8929c299f7e2689ae15923198be887257a758856
mukul34 <55617710+mukul34@users.noreply.github.com>
2020-08-25 22:26:18 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAqw0XD7j2L5IUeXu9icG9S6kZRV_8CA0I",
-  authDomain: "gokutodo-b5dad.firebaseapp.com",
-  databaseURL: "https://gokutodo-b5dad.firebaseio.com",
-  projectId: "gokutodo-b5dad",
-  storageBucket: "gokutodo-b5dad.appspot.com",
-  messagingSenderId: "915714075162",
-  appId: "1:915714075162:web:2fce55a284d5a3051fffa3",
-  measurementId: "G-QVPDRF8QYG",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_DOMAIN",
+  databaseURL: "DATABASE_URL",
+  projectId: "ID",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 });
 
 const db = firebaseApp.firestore();
